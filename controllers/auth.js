cb4f801add4ae37312044e3cdd860318038a4fd6
Carlos Rojas <caranroja@outlook.com>
2023-01-19 16:33:18 -0500

    Se genera login con google autentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const { json } = require('express');
 
 const login = async(req, res) => {
 
@@ -45,9 +47,56 @@ const login = async(req, res) => {
     });
   }
 
+}
+
+const googleSignIn = async(req, res) => {
+  const {id_token} = req.body;
+
+  try {
+
+    const {nombre, correo, img} = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({correo});
+
+    if (!usuario) {
+      //crearlo
+      const data = {
+        nombre,
+        correo,
+        password: ':p',
+        img,
+        google: true
+      }
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    //Si usuario en DB
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      });
+    }
+
+    //generar el jwt
+    const token = await generarJWT(usuario.id);    
+    
+    res.json({
+      usuario,
+      token
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: 'El token no se pudo verificar'
+    })
+  }
 
 }
 
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
